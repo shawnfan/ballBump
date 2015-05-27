c3cbbaa79f7a37a42aef48d97ebd87c3f3bd990c
shawnfan <zhangfan.shawn@gmail.com>
2015-05-26 22:56:02 -0400

    Added touchmove on smartphone browser. Now can control little player on iPhone chrome.
    Move camera into screen: z = 30.

--- js/controls/ObjectMouseControl.js
+++ js/controls/ObjectMouseControl.js
@@ -16,12 +16,22 @@ var ObjectMouseControl = function ( object, spd ) {
 		transY = 0;
 		var x = event.x - window.outerWidth / 2;
 		var y = event.y - window.outerHeight / 2;
-		mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-		mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
-		mouse.z = 0;
-
 
 		//console.log(event);
+		var z = Math.sqrt(x * x + y * y);
+		if (z != 0) {
+			transX = speed * (x / z);
+			transY = speed * (y / z);
+		}
+	};
+
+	//For smartphone control
+	var onTouchMove = function(event) {
+		event.preventDefault();
+		var touch = event.touches[0];
+		var x = touch.pageX - window.innerWidth / 2;
+		var y = touch.pageY - window.innerHeight  / 2;
+
 		var z = Math.sqrt(x * x + y * y);
 		if (z != 0) {
 			transX = speed * (x / z);
@@ -32,12 +42,12 @@ var ObjectMouseControl = function ( object, spd ) {
 	this.update = function() {
 		object.translateX(transX);
 		object.translateY(-transY);
-		return mouse;
 	};
 
 	this.setSpeed = function(spd){
 		speed = spd;
 	};
 	document.addEventListener( 'mousemove', onMouseMove, false );
+	document.addEventListener('touchmove', onTouchMove, false);
 
 };
